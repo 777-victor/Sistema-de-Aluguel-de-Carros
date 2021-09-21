1f2940d194cfea4d7a42d155809ac941d9fb42fa
Victor Campos <victorboaventcampos@gmail.com>
2021-09-20 22:37:39 -0300

    Inicio implementacao
    Projeto com Spring , java 8 pra cima, gradle 7.2  e Mysql

---
+++ implementacao/aluguel/src/main/java/com/pacotes/aluguel/database/Usuario.java
@@ -0,0 +1,30 @@
+package com.pacotes.aluguel.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private String documento;
+
+    private String endereco;
+
+
+}
